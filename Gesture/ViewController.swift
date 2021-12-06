fc95714d1a9b259fffa4faa71cff72a29451dd3f
Anishgamit <89537119+AnishGamit@users.noreply.github.com>
2021-12-06 22:41:27 +0000

    done

--- Gesture/ViewController.swift
+++ Gesture/ViewController.swift
@@ -10,46 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let myv:UIView={
-        let v=UIView()
-        v.backgroundColor = .lightGray
-        v.frame=CGRect(x: 100, y: 100, width: 200, height: 200)
-        return v
+    let myimgv:UIImageView={
+        let img=UIImageView()
+        img.image = UIImage(named: "img.png")
+        img.isUserInteractionEnabled = true  
+        img.frame=CGRect(x: 100, y: 100, width: 200, height: 200)
+        return img
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.addSubview(myv)
+        view.addSubview(myimgv)
         
-       /* let tapgesture=UITapGestureRecognizer(target:self,action: #selector(didtapview))
-        tapgesture.numberOfTapsRequired=2
+        let tapgesture=UITapGestureRecognizer(target:self,action: #selector(tappedImg))
+        tapgesture.numberOfTapsRequired=1
         tapgesture.numberOfTouchesRequired=1
-        myv.addGestureRecognizer(tapgesture)
+        myimgv.addGestureRecognizer(tapgesture)
         
-        let pinchgesture=UIPinchGestureRecognizer(target:self,action: #selector(didpinchview))
-        view.addGestureRecognizer(pinchgesture)
+        let pinchgesture=UIPinchGestureRecognizer(target:self,action: #selector(pinchImg))
+        myimgv.addGestureRecognizer(pinchgesture)
         
-        let rotationgesture=UIRotationGestureRecognizer(target: self, action:#selector(didrotateview))
-        view.addGestureRecognizer(rotationgesture)*/
+        let rotationgesture=UIRotationGestureRecognizer(target: self, action:#selector(rotateImg))
+        view.addGestureRecognizer(rotationgesture)
         
-        /*let lswipe=UISwipeGestureRecognizer(target: self, action:#selector(didswipeview))
+        let lswipe=UISwipeGestureRecognizer(target: self, action:#selector(swipeImg))
         lswipe.direction = .left
         view.addGestureRecognizer(lswipe)
         
-        let rswipe=UISwipeGestureRecognizer(target: self, action:#selector(didswipeview))
+        let rswipe=UISwipeGestureRecognizer(target: self, action:#selector(swipeImg))
         lswipe.direction = .right
         view.addGestureRecognizer(rswipe)
         
-        let uswipe=UISwipeGestureRecognizer(target: self, action:#selector(didswipeview))
+        let uswipe=UISwipeGestureRecognizer(target: self, action:#selector(swipeImg))
         lswipe.direction = .up
         view.addGestureRecognizer(uswipe)
         
-        let dswipe=UISwipeGestureRecognizer(target: self, action:#selector(didswipeview))
+        let dswipe=UISwipeGestureRecognizer(target: self, action:#selector(swipeImg))
         lswipe.direction = .down
-        view.addGestureRecognizer(dswipe)*/
+        view.addGestureRecognizer(dswipe)
         
-        let pangesture=UIPanGestureRecognizer(target: self, action: #selector(didpanview))
+        let pangesture=UIPanGestureRecognizer(target: self, action: #selector(panImg))
         view.addGestureRecognizer(pangesture)
         
     }
@@ -57,36 +58,38 @@ class ViewController: UIViewController {
 
 }
 extension ViewController{
-    @objc private func didtapview(gesture: UITapGestureRecognizer) {
-        print("tapped at location: \(gesture.location(in: myv))")
+    @objc private func tappedImg(gesture: UITapGestureRecognizer) {
+        print("tapped at location: \(gesture.location(in: myimgv))")
+        performSegue(withIdentifier: "segue", sender: self)
     }
-    @objc private func didpinchview(gesture: UIPinchGestureRecognizer) {
-        myv.transform=CGAffineTransform(scaleX: gesture.scale, y: gesture.scale)
+
+    @objc private func pinchImg(gesture: UIPinchGestureRecognizer) {
+        myimgv.transform=CGAffineTransform(scaleX: gesture.scale, y: gesture.scale)
     }
-    @objc private func didrotateview(gesture: UIRotationGestureRecognizer) {
-        myv.transform=CGAffineTransform(rotationAngle: gesture.rotation)
+    @objc private func rotateImg(gesture: UIRotationGestureRecognizer) {
+        myimgv.transform=CGAffineTransform(rotationAngle: gesture.rotation)
     }
-    @objc private func didswipeview(gesture: UISwipeGestureRecognizer) {
+    @objc private func swipeImg(gesture: UISwipeGestureRecognizer) {
         if gesture.direction == .left{
             UIView.animate(withDuration: 0.5){
-            self.myv.frame = CGRect(x: self.myv.frame.origin.x-40, y: self.myv.frame.origin.y, width: 200, height: 200)
+            self.myimgv.frame = CGRect(x: self.myimgv.frame.origin.x-40, y: self.myimgv.frame.origin.y, width: 200, height: 200)
             }
         }
         else  if gesture.direction == .right{
-            myv.frame = CGRect(x: myv.frame.origin.x+40, y: myv.frame.origin.y, width: 200, height: 200)
+            myimgv.frame = CGRect(x: myv.frame.origin.x+40, y: myimgv.frame.origin.y, width: 200, height: 200)
         }
         else  if gesture.direction == .up{
-            myv.frame = CGRect(x: myv.frame.origin.x, y: myv.frame.origin.y-40, width: 200, height: 200)
+            myimgv.frame = CGRect(x: myv.frame.origin.x, y: myimgv.frame.origin.y-40, width: 200, height: 200)
         }
         else  if gesture.direction == .down{
-            myv.frame = CGRect(x: myv.frame.origin.x, y: myv.frame.origin.y+40, width: 200, height: 200)
+            myimgv.frame = CGRect(x: myv.frame.origin.x, y: myimgv.frame.origin.y+40, width: 200, height: 200)
         }
     }
     
-    @objc private func didpanview(gesture: UIPanGestureRecognizer) {
+    @objc private func panImg(gesture: UIPanGestureRecognizer) {
        let x = gesture.location(in: view).x
         let y = gesture.location(in: view).y
-        myv.center=CGPoint(x: x, y: y)
+        myimgv.center=CGPoint(x: x, y: y)
     }
     
     
